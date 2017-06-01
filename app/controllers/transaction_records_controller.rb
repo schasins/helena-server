c9722e1feeae17dc19e3da394f9dd8738fadd53d
schasins <schasins@gmail.com>
2017-06-01 16:29:06 -0700

    Recency constraint information can now be interpreted by the method that handles checking for whether a transaction exists.

--- app/controllers/transaction_records_controller.rb
+++ app/controllers/transaction_records_controller.rb
@@ -22,9 +22,34 @@ class TransactionRecordsController < ApplicationController
     annotation_id = params[:annotation_id]
     commit_time = Time.at(params[:commit_time].to_i/1000)
 
+    physical_time_diff_seconds = (params[:physical_time_diff_seconds])
+    logical_time_diff = (params[:logical_time_diff])
+
     # this is tricky.  need to select transactions based on having all the cells we want
     transaction_query = TransactionRecord.where(program_id: program_id, annotation_id: annotation_id)
-    # for here we only filter based on having the same program, but can also use commit time and program_run to filter
+    
+    # above only filters based on having the same program, but can also use time and program_run to filter
+    if (physical_time_diff_seconds)
+      # use time (physical time)
+      physical_time_diff_seconds = physical_time_diff_seconds.to_i
+      threshold_time = Time.now - physical_time_diff_seconds
+      transaction_query = transaction_query.where("commit_time > ?", threshold_time)
+    elsif (logical_time_diff)
+      # use program run (logical time)
+      logical_time_diff = logical_time_diff.to_i
+      # ok, for this we need to figure out what program runs we've done for the program.
+      # if logical_time_diff is 2, we're allowed to skip if we've seen a duplicate in the last two runs
+      # that is, this run plus either of the last 2
+      runs = ProgramRun.where(program_id: program_id).order(created_at :desc)
+      if (runs.size < (logical_time_diff + 1))
+        # don't actually need to do any filtering.  the whole history of the executions falls in our target range
+      else
+        # if the logical_time_diff is 2, we'll skip if we see duplicates in the curr run or prior 2, so we're
+        # interested in the set of transaction records made after the the time when the third prog run in runs was created
+        threshold_time = runs[logical_time_diff].created_at
+        transaction_query = transaction_query.where("commit_time > ?", threshold_time)
+      end
+    end
 
     transaction_items = JSON.parse(URI.decode(params[:transaction_attributes]))
     index = -1
