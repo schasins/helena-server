32481a26638707079d68597b4c19e040a54dd7db
schasins <schasins@gmail.com>
2019-11-12 11:22:05 -0500

    Reordering routes.

--- config/routes.rb
+++ config/routes.rb
@@ -37,9 +37,9 @@ Rails.application.routes.draw do
   get 'datasets/run/:id' => 'program_runs#download_run', :defaults => { :format => 'csv' }
   get 'datasets/rundetailed/:id' => 'program_runs#download_run_detailed', :defaults => { :format => 'csv' }
   get 'datasets/:id' => 'program_runs#download_all_runs', :defaults => { :format => 'csv' }
+  get 'datasets/:id/detailed' => 'program_runs#download_all_runs', :defaults => { :format => 'csv', :detailed => true }
   get 'datasets/:id/:hours' => 'program_runs#download_all_runs', :defaults => { :format => 'csv' }
   get 'datasets/:id/lastxrows/:rows' => 'program_runs#download_all_runs', :defaults => { :format => 'csv' }
-  get 'datasets/:id/detailed' => 'program_runs#download_all_runs', :defaults => { :format => 'csv', :detailed => true }
 
   post 'saveprogram', :to => 'programs#save_program', :as => 'saveprogram'
 
